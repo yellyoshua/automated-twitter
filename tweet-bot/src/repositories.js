d4cee7306689ac1906ccf20d0ed53e9d48a6292f
yellyoshua <usuario1529yoshua@hotmail.com>
2022-06-03 17:02:10 +0000

    exporting repositories

--- tweet-bot/src/repositories.js
+++ tweet-bot/src/repositories.js
@@ -1,8 +1,10 @@
 import repositoryController from './controllers/repository.controller.js';
-import userModel from "./models/user.model.js";
-import sessionModel from "./models/session.model.js";
+import userModel from "./models/users.model.js";
+import sessionModel from "./models/sessions.model.js";
+import tweetsModel from "./models/tweets.model.js";
 
 export default {
-    userRepository: repositoryController(userModel),
-    sessionRepository: repositoryController(sessionModel),
-}
+    usersRepository: repositoryController(userModel),
+    sessionsRepository: repositoryController(sessionModel),
+    tweetsRepository: repositoryController(tweetsModel),
+};
